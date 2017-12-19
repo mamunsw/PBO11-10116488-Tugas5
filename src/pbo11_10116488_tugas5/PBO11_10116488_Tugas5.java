c717b605701ebc4b7f3a04d9cd18000a362346e0
MSW98 <MSW98@DESKTOP-4BCNEIF>
2017-12-19 20:09:35 +0700

    PBO11-10116488-Tugas5

---
+++ src/pbo11_10116488_tugas5/PBO11_10116488_Tugas5.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11_10116488_tugas5;
+
+/**
+ *
+ * @author MSW98
+ */
+public class PBO11_10116488_Tugas5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
